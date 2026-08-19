b5eb927e0407640248ab8f179b29ed3f0efcc1a6
Ahmed Gaber <ahmedgaber1114@gmail.com>
2026-08-20 00:32:14 +0300

    style: update favicon and remove stale build cache

--- .next.old/server/chunks/ssr/_next-internal_server_app_(public)_projects_page_actions_1edyxxr.js
+++
@@ -1,3 +0,0 @@
-module.exports=[17228,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_%28public%29_projects_page_actions_1edyxxr.js.map
--- .next.old/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"J4+lKYUX+wi6nkF0hXPBWbqlBjST7aIQbujQhBQTKpU=\"\n}"
